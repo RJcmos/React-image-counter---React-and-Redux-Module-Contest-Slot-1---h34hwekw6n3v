0ade64442fdaed6128639acc7bdd5e5570db8a05
Rahul Jadhav <jadhav.rj.cmos@gmail.com>
2023-10-15 11:18:12 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/components/App.js"
+            template: "./src/index.html"
         })
     ],
     resolve: {
