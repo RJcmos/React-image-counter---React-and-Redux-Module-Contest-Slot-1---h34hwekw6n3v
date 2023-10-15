e8a32a56ca1ca1750ea67cbb7cc957898b1eac7d
Rahul Jadhav <jadhav.rj.cmos@gmail.com>
2023-10-15 11:08:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
+  const [size,setSize]=userState(300)
+  const handleClick=()=>{
+    setSize(size+2)
+  }
+  
   return (
     <div id="main">
-      <img src={star}  height="300px" width="300px" />
+      <img src={star}  
+         height={'${size}px'}
+         width={'${size}px'}
+      />
     </div>
   )
 }
